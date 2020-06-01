dd8284a3cf12b63ec6598fb8a245548f750e185a
Talieva Elena <erulen@mail.ru>
2020-06-02 00:00:21 +0300

    implement methods "enc" and "dec"

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -3,22 +3,30 @@ package encryptdecrypt;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine().toLowerCase();
+        String operation = in.nextLine().toLowerCase();
+        String str = in.nextLine();
         int key = in.nextInt();
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        if (operation.matches("enc")) {
+            enc(str, key);
+        } else {
+            dec(str, key);
+        }
+    }
+
+    private static void dec(String str, int key) {
         for (int i = 0; i < str.length(); i++) {
-            System.out.print(str.substring(i, i));
-            if (str.substring(i, i + 1).matches("[a-z]")) {
-                int encryptionIndex = alphabet.indexOf(str.substring(i, i + 1)) + key;
-                while (encryptionIndex >= alphabet.length() - 1) {
-                    encryptionIndex = encryptionIndex - 26;
-                }
-                System.out.print(alphabet.charAt(encryptionIndex));
-            } else {
-                System.out.print(str.substring(i, i + 1));
-            }
+            int decryptionIndex = (int) str.charAt(i) - key;
+            System.out.print((char) decryptionIndex);
+        }
+    }
+
+    private static void enc(String str, int key) {
+        for (int i = 0; i < str.length(); i++) {
+            int encryptionIndex = (int) str.charAt(i) + key;
+            System.out.print((char) encryptionIndex);
         }
     }
 }
